0e162a5324ef0ea8b801eba315e4e5320d7707f5
JmTameta <tametaj1@tcnj.edu>
2021-02-27 14:07:02 -0500

    changed the template

---
+++ config.py
@@ -0,0 +1,10 @@
+# Can be multiple prefixes, like this: ("!", "?")
+BOT_PREFIX = ("!")
+TOKEN = "ODE1Mjg4NjYzNzY3MjUzMDM1.YDqOvA.cDaS2JFcCrUJFhK3Bqhca-UZzVE"
+APPLICATION_ID = "815288663767253035"
+OWNERS = [123456789, 987654321]
+BLACKLIST = []
+ # Default cogs that I have created for the template
+STARTUP_COGS = [
+    "cogs.general", "cogs.help", "cogs.moderation", "cogs.owner",
+]
